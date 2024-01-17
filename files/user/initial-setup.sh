25dc02781004db3944f468ab6be087e4801499c0
Benjamin Krein <superbenk@gmail.com>
2024-01-17 00:24:40 -0500

    tweaks to desktop setup

--- files/user/initial-setup.sh
+++ files/user/initial-setup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVirtual-1/workspace0/last-image -s "/home/$USER/Pictures/wallpaper.png"
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVirtual-1/workspace0/rgba1 -s 0.141176 -s 0.121569 -s 0.192157 -s 1.000000
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVirtual-1/workspace0/rgba2 -s 0.466667 -s 0.462745 -s 0.482353 -s 1.000000
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVirtual-1/workspace0/color-style -s 2
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVirtual-1/workspace0/image-style -s 4
-xfconf-query -c xfce4-desktop -p /backdrop/single-workspace-mode -s true
-xfconf-query -c xfce4-desktop -p /backdrop/single-workspace-number -s 0
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVirtual-1/workspace0/last-image -t string -s "/home/$USER/Pictures/wallpaper.png"
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVirtual-1/workspace0/rgba1 -t float -t float -t float -t float -s 0.141176 -s 0.121569 -s 0.192157 -s 1.000000
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVirtual-1/workspace0/rgba2 -t float -t float -t float -t float -s 0.466667 -s 0.462745 -s 0.482353 -s 1.000000
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVirtual-1/workspace0/color-style -t int -s 2
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVirtual-1/workspace0/image-style -t int -s 4
+xfconf-query -c xfce4-desktop -p /backdrop/single-workspace-mode -t bool -s true
+xfconf-query -c xfce4-desktop -p /backdrop/single-workspace-number -t int -s 0
