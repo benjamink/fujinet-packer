f1383195e3ee8b8268c28e869780c8509dfdfb2f
Benjamin Krein <superbenk@gmail.com>
2024-02-02 17:14:59 -0500

    make build script smarter

--- packer_build.sh
+++ packer_build.sh
@@ -1,8 +1,31 @@
 #!/usr/bin/env bash
 
-VERSION="${1:-test}"
+VERSION="test" 
+LOCAL_ONLY="False"
+
+function usage() {
+  echo "$0 [-v version] [-l] [-h]"
+  echo
+  echo "  -v <version>      Provide a version for the VM build (default: 'test')"
+  echo "  -l                If set build WILL NOT be uploaded to MEGA (local only)" 
+  echo "  -h                Display this help"
+  echo
+  exit 1
+}
+
+while getopts "v:lh" opt
+do
+  case "$opt" in
+    v) VERSION="$OPTARG" ;;
+    l) LOCAL_ONLY="true" ;;
+    h) usage ;;
+  esac
+done
 
 echo PACKER_DEBUG=1 packer build -var="vm_version=$VERSION" -force -color=false -machine-readable -on-error=abort . | tee packer.out
 
-echo mega-put output-qemu/fujinet-debian12.ova "FujiNet/VirtualMachine/fujinet-debian12-vbox-$VERSION.ova"
+if [[ $LOCAL_ONLY != "true" ]]
+then 
+  echo mega-put output-qemu/fujinet-debian12.ova "FujiNet/VirtualMachine/fujinet-debian12-vbox-$VERSION.ova"
+fi
 
