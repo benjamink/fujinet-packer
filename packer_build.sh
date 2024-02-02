65483f3a13fc1ba79caec46e81bf11fb7b057db9
Benjamin Krein <superbenk@gmail.com>
2024-02-02 17:16:57 -0500

    enable build script commands

--- packer_build.sh
+++ packer_build.sh
@@ -22,10 +22,9 @@ do
   esac
 done
 
-echo PACKER_DEBUG=1 packer build -var="vm_version=$VERSION" -force -color=false -machine-readable -on-error=abort . | tee packer.out
+PACKER_DEBUG=1 packer build -var="vm_version=$VERSION" -force -color=false -machine-readable -on-error=abort . | tee packer.out
 
 if [[ $LOCAL_ONLY != "true" ]]
 then 
-  echo mega-put output-qemu/fujinet-debian12.ova "FujiNet/VirtualMachine/fujinet-debian12-vbox-$VERSION.ova"
+  mega-put output-qemu/fujinet-debian12.ova "FujiNet/VirtualMachine/fujinet-debian12-vbox-$VERSION.ova"
 fi
-
