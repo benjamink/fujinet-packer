ac14269cbd007b74232680df7589c43769733eca
Benjamin Krein <superbenk@gmail.com>
2024-02-06 09:16:10 -0500

    remove proc restart

--- packer_build.sh
+++ packer_build.sh
@@ -1,30 +1,36 @@
 #!/usr/bin/env bash
 
 VERSION="test" 
-LOCAL_ONLY="False"
+COPY_ONLY="false"
+LOCAL_ONLY="false"
 
 function usage() {
-  echo "$0 [-v version] [-l] [-h]"
+  echo "$0 [-v version] [-l] [-c] [-h]"
   echo
   echo "  -v <version>      Provide a version for the VM build (default: 'test')"
   echo "  -l                If set build WILL NOT be uploaded to MEGA (local only)" 
+  echo "  -c                Just copy the OVA to MEGA"
   echo "  -h                Display this help"
   echo
   exit 1
 }
 
-while getopts "v:lh" opt
+while getopts "v:lch" opt
 do
   case "$opt" in
     v) VERSION="$OPTARG" ;;
     l) LOCAL_ONLY="true" ;;
+    c) COPY_ONLY="true" ;;
     h) usage ;;
   esac
 done
 
-PACKER_DEBUG=1 packer build -var="vm_version=$VERSION" -force -color=false -machine-readable -on-error=abort . | tee packer.out
+if [[ "$COPY_ONLY" != "true" ]]
+then
+  PACKER_DEBUG=1 packer build -var="vm_version=$VERSION" -force -color=false -machine-readable -on-error=clean . | tee packer.out
+fi
 
-if [[ $LOCAL_ONLY != "true" ]]
+if [[ "$LOCAL_ONLY" != "true" ]]
 then 
   mega-put "output/fujinet-debian12-vbox.ova" "FujiNet/VirtualMachine/fujinet-debian12-vbox.ova"
 fi
