8acc9896323c49e554803f5e317ad7ec1bf3ce17
Benjamin Krein <superbenk@gmail.com>
2024-02-22 14:24:05 -0500

    clean up clutter

--- scripts/xfce-fujinet-menu.sh
+++ scripts/xfce-fujinet-menu.sh
@@ -9,9 +9,9 @@ mkdir -p "$APPS_ROOT_PATH"
 cp /etc/xdg/menus/xfce-applications.menu "$MENU_ROOT_PATH"
 sed -i 's/Other/FujiNet/;s/xfce-other/xfce-fujinet/' "$MENU_ROOT_PATH/xfce-applications.menu"
 
-FN_WIKI_ICON_PATH="/home/$P_USERNAME/Desktop/FujiNet-Wiki-URL.desktop"
-VM_HELP_ICON_PATH="/home/$P_USERNAME/Desktop/FujiNet-VM-Help-URL.desktop"
-APPLEWIN_HELP_ICON_PATH="/home/$P_USERNAME/Desktop/AppleWin-Help-URL.desktop"
+FN_WIKI_ICON_PATH="/home/$P_USERNAME/.local/share/applications/FujiNet-Wiki-URL.desktop"
+VM_HELP_ICON_PATH="/home/$P_USERNAME/.local/share/applications/FujiNet-VM-Help-URL.desktop"
+APPLEWIN_HELP_ICON_PATH="/home/$P_USERNAME/.local/share/applications/AppleWin-Help-URL.desktop"
 
 cat <<EOF | tee "$VM_HELP_ICON_PATH"
 [Desktop Entry]
@@ -52,4 +52,4 @@ EOF
 chmod +x "$FN_WIKI_ICON_PATH"
 gio set -t string "$FN_WIKI_ICON_PATH" metadata::xfce-exe-checksum "$(sha256sum "$FN_WIKI_ICON_PATH" | awk '{print $1}')"
 
-cp "/home/$P_USERNAME/Desktop/*.desktop" "$APPS_ROOT_PATH/"
+cp /home/"$P_USERNAME"/Desktop/*.desktop "$APPS_ROOT_PATH/"
